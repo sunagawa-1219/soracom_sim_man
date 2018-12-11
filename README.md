855007e968346c16f98e80487fbfbb4ce024bcb2
sunagawa <naoko_sunagawa@kyoso.co.jp>
2018-12-11 11:16:00 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# soracom_sim_man
-make kintone app to manage SORACOM Air SIM status
+# SORACOM SIM 自動管理アプリ
+
+## 概要
+
+SORACOM SIM を自動管理できる kintone アプリのための lambda コードです。
+
+アプリの構成などについては qiita 記事を御覧ください。
+
+## 開発環境
+
+- Node.js8.10
+
+## 環境変数について
+
+セキュリティのため、以下の情報は環境変数に持たせています。
+
+- KINTONE_TOKEN : kintone api を使用するのに必要なトークン
+- USER_NAME : kintone の basic 認証に必要なユーザー名
+- PASSWORD : kintone の basic 認証に必要なパスワード
+- SUB_DOMAIN : kintone のサブドメイン
+- APP_ID : 対象の kintone のアプリ ID
+- SORACOM_AUTH_KEY : SORACOM API を使用するために使う SORACOM ユーザーの認証キー ID
+- SORACOM_SECRET_KEY : SORACOM ユーザーのシークレットキー
---
+++ index.js
@@ -0,0 +1,31 @@
+const SoracomController = require("./lib/soracom_controller");
+const KintoneController = require("./lib/kintone_controller");
+const RequestController = require("./lib/request_controller");
+const RecordsController = require("./lib/records_controller");
+
+exports.handler = async (event, context) => {
+  try {
+    const records = await KintoneController.scanKintoneRecords();
+    const webClient = RequestController.makeWebClient();
+    const { apiKey, token } = await SoracomController.getToken(webClient);
+    const soracomController = new SoracomController(token, apiKey);
+    await soracomController.scanSimInfo();
+    await soracomController.scanGroups();
+    const summarizedSimInfo = soracomController.summarizeSimInfo();
+    const checkedRecords = RecordsController.makeRecords(
+      summarizedSimInfo,
+      records
+    );
+    await KintoneController.changeAllRecords(
+      checkedRecords.addRecords,
+      checkedRecords.updateRecords,
+      checkedRecords.removeRecordIds
+    );
+    console.log("ALL DONE");
+    return "success!";
+  } catch (err) {
+    console.log("error happen");
+    console.error(err);
+    return "error";
+  }
+};
